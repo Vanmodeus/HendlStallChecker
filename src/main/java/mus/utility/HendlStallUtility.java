c310d63df1faf8a42db30496b75fa98b8eab9339
Reinhart Herzog <reinhart_herzog@yahoo.de>
2016-06-11 13:59:05 +0200

    CHG Utility for rasp ip

--- src/main/java/mus/utility/HendlStallUtility.java
+++ src/main/java/mus/utility/HendlStallUtility.java
@@ -5,13 +5,25 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class HendlStallUtility {
+	private static final String PROP_FILENAME = "config.properties";
+	
 	public static String getWebcamIP() throws Exception {
 		Properties prop = new Properties();
-		String propFileName = "config.properties";
+		String propFileName = PROP_FILENAME;
 		InputStream inputStream = new FileInputStream(propFileName);
 		prop.load(inputStream);
 
 		String ipcamAddress = prop.getProperty("ipcamAddress");
+		return ipcamAddress;
+	}
+	
+	public static String getDbIP() throws Exception {
+		Properties prop = new Properties();
+		String propFileName = PROP_FILENAME;
+		InputStream inputStream = new FileInputStream(propFileName);
+		prop.load(inputStream);
+
+		String ipcamAddress = prop.getProperty("dbAddress");
 		return ipcamAddress;
 	}
 }
