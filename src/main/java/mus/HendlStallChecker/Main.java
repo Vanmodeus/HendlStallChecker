e5f1ed972034fecc9706159b7d16b0720b87b8d1
Reinhart Herzog <reinhart_herzog@yahoo.de>
2016-06-11 16:37:00 +0200

    CHG config

--- src/main/java/mus/HendlStallChecker/Main.java
+++ src/main/java/mus/HendlStallChecker/Main.java
@@ -1,8 +1,18 @@
 package mus.HendlStallChecker;
 
+import java.util.Properties;
+
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
+		
+		//log level
+		Properties p = new Properties(System.getProperties());
+		p.put("com.mchange.v2.log.MLog", "com.mchange.v2.log.FallbackMLog");
+		p.put("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL", "OFF"); // Off or any other level
+		System.setProperties(p);
+		
+		
 		HendlStallVerwalter admin = new HendlStallVerwalter();
 		
 		admin.startOpenDoorThread();
