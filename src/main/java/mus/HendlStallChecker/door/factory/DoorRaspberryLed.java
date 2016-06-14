5c666c40a9cf73ed055517dd0ab01e0205028c62
Reinhart Herzog <reinhart_herzog@yahoo.de>
2016-06-14 21:28:00 +0200

    fix led

--- src/main/java/mus/HendlStallChecker/door/factory/DoorRaspberryLed.java
+++ src/main/java/mus/HendlStallChecker/door/factory/DoorRaspberryLed.java
@@ -5,17 +5,15 @@ import com.pi4j.io.gpio.RaspiPin;
 import mus.utility.PlatformHelper;
 
 public class DoorRaspberryLed implements IDoor {
+	public void openDoor() {
+		System.out.println("opening door...");
+		
+		PlatformHelper.touchLed(RaspiPin.GPIO_00, true);
+	}
 
 	public void closeDoor() {
 		System.out.println("closing door...");
 
 		PlatformHelper.touchLed(RaspiPin.GPIO_00, false);
 	}
-
-	public void openDoor() {
-		System.out.println("opening door...");
-
-		PlatformHelper.touchLed(RaspiPin.GPIO_00, false);
-	}
-
 }
